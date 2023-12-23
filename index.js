b97c6430509f943758deb6bc4e2d86b0e969945c
RosaliaNjoki <njosali0@gmail.com>
2023-12-24 00:05:12 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(array){
+    const members = ["Guadalupe", "Ollie", "Aki"];
+    const message = [];
+
+        for (let i = 0; i < members.length; i++){
+        
+  const surpriseMessage = `Thank you, ${members[i]}, for the wonderful surprise gift!`;
+  message.push(surpriseMessage);
+    }
+return message;
+     
+}
+
+function countDown(count){
+   while (count > 0) {
+       console.log(count);
+        count--;
+    }
+    console.log(count); 
+
+ }
